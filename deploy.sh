e60b2a4a3cfe8674df8e30f95711d65d9eaa7f0a
Mayuresh Rajwadkar <m.m.rajwadkar@ieee.org>
2018-06-08 15:01:25 -0400

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@
 export LC_TIME=en_US.UTF-8
 
 # The commit message.
-MESSAGE="Site rebuild $(date)"
+MESSAGE="Site rebuild $(date) / deploy.sh"
 
 msg() {
     printf "\033[1;32m :: %s\n\033[0m" "$1"
